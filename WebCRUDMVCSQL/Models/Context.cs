5424ef319af8c42809ff5306f86fe93730361475
x340348 <Tty_Erick.Alencar@mercantil.com.br>
2022-02-22 14:06:26 -0300

    Initial commit

---
+++ WebCRUDMVCSQL/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace WebCRUDMVCSQL.Models
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+
+        {
+
+        }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
